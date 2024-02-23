7572e00d72183883d49b96e7821488521f6e9193
fahmyadan <fahmy.adan14@imperial.ac.uk>
2024-02-23 15:52:40 +0000

    add custom actor critic cnn policy to registry

--- stable_baselines3/ppo/policies.py
+++ stable_baselines3/ppo/policies.py
@@ -1,7 +1,9 @@
 # This file is here just to define MlpPolicy/CnnPolicy
 # that work for PPO
-from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, MultiInputActorCriticPolicy
+from stable_baselines3.common.policies import ActorCriticCnnPolicy, ActorCriticPolicy, MultiInputActorCriticPolicy, CustomActorCriticCnnPolicy
 
 MlpPolicy = ActorCriticPolicy
 CnnPolicy = ActorCriticCnnPolicy
 MultiInputPolicy = MultiInputActorCriticPolicy
+CustomCnnPolicy = CustomActorCriticCnnPolicy
+
